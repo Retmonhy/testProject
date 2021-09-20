0dc166e9203684d34da4c6d9e78ce700dda3e982
retmonhy <retmonhy@gmail.com>
2021-09-21 00:00:34 +0700

    Сделан header+адаптив

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+// console.log(lottie);
+
+// const anim = lottie;
+
+// anim.loadAnimation({
+// 	container: document.getElementById('animation-div'),
+// 	renderer: 'svg',
+// 	loop: true,
+// 	autoplay: false,
+// 	path: 'js/MixerAnimation/MixerSequence.json',
+// });
+
+// $('.body').on('scroll', function(playAnimation){
+// 	anim.play();
+// });
+
+
+
+/*let theWindow = $(window);
+let winHeight = theWindow.height();
+let animDuration = winHeight * 4;
+let rotating = {
+	container: document.getElementById('animation-div'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: false,
+	path: 'js/MixerAnimation/MixerSequence.json',
+};
+
+var anim = bodymovin.loadAnimation(rotating);
+
+$(window).on('scroll', function(){
+	animatebodymovin(animDuration, anim);
+});
+
+function animatebodymovin(duration, animObject){
+	let scrollPosition = theWindow.scrollTop();
+	let maxFrames = animObject.totalFrames;
+	let frame = (maxFrames / 150) * (scrollPosition / (duration / 100));
+
+	animObject.goToAndStop(frame, true);
+}*/
